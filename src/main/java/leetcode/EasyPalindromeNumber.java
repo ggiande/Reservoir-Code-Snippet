24e1b18bc04b82bfe4aef10710e5d826e7e17485
ggiande <ggarciadeleon@csustan.edu>
2021-08-05 13:27:25 -0700

    New Leetcode Question

---
+++ src/main/java/leetcode/EasyPalindromeNumber.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class EasyPalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+
+    public static boolean isPalindrome(int x) {
+        int input = x;
+        if (x < 0){
+            return false;
+        }
+        int reversedNum = 0;
+        // Reverse the number -- % 10 grabs the last digit, / 10 removes the last digit
+        while (x != 0) {
+            int temp = x % 10;
+            x /= 10;
+            reversedNum = reversedNum * 10 + temp;
+        }
+        // Compare the reversed number with the original number
+        if (reversedNum == input){
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/leetcode/IncompleteEasyReverseInteger.java
+++ src/main/java/leetcode/IncompleteEasyReverseInteger.java
@@ -2,28 +2,26 @@ package leetcode;
 
 public class IncompleteEasyReverseInteger {
     public static void main(String[] args) {
-        int x = 123;
-
-        System.out.println(x % 10);
-        // return the integer
+        int x = -123;
+        System.out.println(reverse(x));
     }
 
-    public int reverse(int x) {
+    public static int reverse(int x) {
         boolean negativeInt = false;
         if (x < 0) {
             // negative, use abs, but check a boolean
             negativeInt = true;
             x = Math.abs(x);
         }
-        //123
-        int temp = 0;
-        int currRev = 0;
-        while (x % 10 != 0){
-            temp = x % 10; // 3
-            currRev =+ temp;
 
-
+        int reversedNum = 0;
+        // Reverse the number -- % 10 grabs the last digit, / 10 removes the last digit
+        while (x != 0) {
+            int temp = x % 10;
+            x /= 10;
+            reversedNum = reversedNum * 10 + temp;
         }
-        return -1;
+        if (negativeInt) return reversedNum * -1;
+        return reversedNum;
     }
 }
